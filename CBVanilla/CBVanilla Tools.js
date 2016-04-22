2729de14106936fa7a24a59b33e8ae0e8240f960
estasney <ericstasney@gmail.com>
2016-04-21 22:55:02 -0600

    Update CBVanilla Tools.js
    Add "add to email campaign" to launch another google form. Used comma delimiters to reduce code.
    
    Use function to search for "present" in the current job. If it exists, return "Active" otherwise "Past". Useful for personalizing emails.

--- CBVanilla/CBVanilla Tools.js
+++ CBVanilla/CBVanilla Tools.js
@@ -32,21 +32,23 @@ buttonMake("Salesforce Search", target, "searchbutton");
 buttonMake("Log", target, "googlelog");
 buttonMake("Upload to SF", target, "upload");
 buttonMake("Edge Search", target, "edgesearch");
+buttonMake("Add to Campaign", target, "emailcampaign");
 //turn them into jQuery buttons
 $('.mybuttons').button();
 //assign function to buttons
 
 //CSS styling for the buttons
 var cssButtons = {
-    position: "fixed",
-    right: "0",
-    width: "200px"
+	position: "fixed",
+	right: "0",
+	width: "200px"
 };
 $('.mybuttons').css(cssButtons);
 $('#searchbutton').css("top", "0px");
 $('#googlelog').css("top","40px");
 $('#upload').css("top", "80px");
 $('#edgesearch').css("top", "120px");
+$('#emailcampaign').css("top", "160px");
 //function to search for candidate name in SalesForce
 function salesSearch() {
 	var fullName = $("#personalinfo > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(2) > div").text().trim().split(" ");
@@ -89,15 +91,40 @@ function parseRes () {
 	window.open("https://c.na28.visual.force.com/apex/SovrenProcessDocument?sfdc.tabName=01rC0000000IVe7");
 }
 $("#upload").on("click", parseRes);
-
+//function to perform a CB Edge Search
 function edgesearch() {
-	var fullName = $("#personalinfo > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(2) > div").text().trim().split(" "); 
+	var fullName = $("#personalinfo > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(2) > div").text().trim().split(" ");
 	var location = $("td.lbl.right:contains('Location:') + td").text().trim().split("-");
 	var city = location[2];
 	var url = "http://edge.careerbuilder.com/search?Names=" + fullName[0] +"%20" + fullName[1] + "&" + "Location=" + city;
 	window.open(url);
 }
 $("#edgesearch").on("click", edgesearch);
+//function to log the candidate to a seperate Google Form for email campaigns
+function emailcampaign() {
+	var name = $("#personalinfo > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(2) > div").text().trim();
+	//uses the Phone column to find the phone number... genius!
+	var phone = $("td.lbl.right:contains('Phone:') + td").text().trim();
+	var email = $("#EmailLink").text().trim();
+	var zip = $("td.lbl.right:contains('Location:') + td").text().trim().split("-")[3];
+	var lastMod = $("td.lbl.right:contains('Last Modified:') + td").text().trim();
+	var lastAct = $("td.lbl.right:contains('Last Activity:') + td").text().trim();
+	var currentEmp = $("#workhistory > table > tbody > tr:nth-child(1) > td:nth-child(2)").text().trim();
+	var title = $("#workhistory > table > tbody > tr:nth-child(2) > td:nth-child(2)").text().trim();
+	var length = $("#workhistory > table > tbody > tr:nth-child(1) > td:nth-child(3)").text().trim();
+	function currentOrPast (cssTarget) {
+		if (cssTarget.indexOf("Present")) {
+			return "Active";
+			} else {
+			return "Past";
+			}
+	}
+	var formURL = "https://docs.google.com/forms/d/1p70030M_I4WbEIFUMbzmtoaAEFGX5pdrn9SOO6WTr84/viewform?";
+	window.open(formURL + "entry.701181576=" + name + "&entry.853252475=" + phone + "," + email + "," + zip + "," + "&entry.1180331322=" + lastMod + "," + lastAct + "&entry.2044582287=" + currentEmp + "," + title+ "," + length + "," + currentOrPast(length));
+	var candURL = window.location.href;
+}
+$("#emailcampaign").on("click", emailcampaign);
+
 
 
 
